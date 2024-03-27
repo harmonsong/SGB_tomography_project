7ec77b237c74735f3e28342714e7c6e456cac100
Harmon at FJ server <2947438278@qq.com>
2024-03-27 09:29:46 +0800

    SGB v9.5.6: revise for email_sh automation script.

--- ModelValidCode/ModelValidCode/wave_forward/email_sh.py
+++ ModelValidCode/ModelValidCode/wave_forward/email_sh.py
@@ -8,7 +8,7 @@ import os
 # email config
 user_send = '2947438278@qq.com'
 user_recive = '2947438278@qq.com'
-password = 'mzltobvihgeidgih'
+password = 'wmmnxrzfusqbddbi'
 smtp_server = 'smtp.qq.com'
 yag_server = yagmail.SMTP(user= user_send, password= password, host= smtp_server)
 dir_origin = os.getcwd()
@@ -22,10 +22,10 @@ def submit_job(user_recive, yag_server,flag_this,jobs_status,dir_origin):
         results = subprocess.run(shell_order, shell=True, check=True, capture_output=True,text=True)
         output = results.stdout.strip()
         job_id = output.split()[1][1:-1]
-        print("Job submitted successfully! Job ID: ", job_id, ' source: ', str(flag_this+1))
+        print("Job submitted successfully! Job ID: ", job_id, '; source: ', str(flag_this+1))
         email_to = [user_recive]
         email_title = 'Taiyi Job'
-        email_content = 'Job submitted successfully! Job ID: '+job_id+ ' source: '+ str(flag_this+1)
+        email_content = 'Job submitted successfully! Job ID: '+job_id+ '; source: '+ str(flag_this+1)
         yag_server.send(email_to, email_title, email_content)
         jobs_status[flag_this] = 1
     except subprocess.CalledProcessError as e:
@@ -45,36 +45,28 @@ def check_status(jobs_status,jobs_ids):
         job_id = jobs_ids[flag_run]
         results = subprocess.run('bjobs '+job_id, shell=True, check=True, capture_output=True,text=True)
         output = results.stdout.strip()
-        if 'not found' in output:
+        state = output.split()[10]
+        if state == 'DONE':
             email_to = [user_recive]
             email_title = 'TaiYi Job'
             email_content = 'Job finished successfully! Job ID: '+job_id + ' source: '+ str(flag_run+1)
             yag_server.send(email_to, email_title, email_content)
             print("Job finished successfully! Job ID: ", job_id, ' source: ', str(flag_run+1))
             jobs_status[flag_run] = 3
-        else:
-            state = output.split()[10]
-            if state == 'DONE':
-                email_to = [user_recive]
-                email_title = 'TaiYi Job'
-                email_content = 'Job finished successfully! Job ID: '+job_id + ' source: '+ str(flag_run+1)
-                yag_server.send(email_to, email_title, email_content)
-                print("Job finished successfully! Job ID: ", job_id, ' source: ', str(flag_run1))
-                jobs_status[flag_run] = 3
-            elif state == 'EXIT':
-                email_to = [user_recive]
-                email_title = 'TaiYi Job'
-                email_content = 'Job finished fail! Job ID: '+job_id + ' source: '+ str(flag_run)
-                yag_server.send(email_to, email_title, email_content)
-                print("Job finished fail! Job ID: ", job_id, ' source: ', str(flag_run))
-                jobs_status[flag_run] = -1
-            elif state == 'RUN' and jobs_status[flag_run] == 1:
-                email_to = [user_recive]
-                email_title = 'TaiYi Job'
-                email_content = 'Job Start running! Job ID: '+job_id
-                yag_server.send(email_to, email_title, email_content)
-                print("Job start running! Job ID: ", job_id, ' source: ', str(flag_run))
-                jobs_status[flag_run] = 2
+        elif state == 'EXIT':
+            email_to = [user_recive]
+            email_title = 'TaiYi Job'
+            email_content = 'Job finished fail! Job ID: '+job_id + ' source: '+ str(flag_run+1)
+            yag_server.send(email_to, email_title, email_content)
+            print("Job finished fail! Job ID: ", job_id, ' source: ', str(flag_run+1))
+            jobs_status[flag_run] = -1
+        elif state == 'RUN' and jobs_status[flag_run] == 1:
+            email_to = [user_recive]
+            email_title = 'TaiYi Job'
+            email_content = 'Job Start running! Job ID: '+job_id
+            yag_server.send(email_to, email_title, email_content)
+            print("Job start running! Job ID: ", job_id, ' source: ', str(flag_run+1))
+            jobs_status[flag_run] = 2
     return jobs_status
 
 flag_max = 10
@@ -82,7 +74,7 @@ flag_jobs = 63 #63
 jobs_status = np.zeros(flag_jobs) # 0--not start, 1--pend, 2--run, 3--finished, -1--fail
 jobs_ids = {}
 
-flag_this = 0
+flag_this = 33 # source_num -1
 while flag_this < flag_jobs:
     # submit job
     job_id,jobs_status = submit_job(user_recive, yag_server,flag_this,jobs_status,dir_origin)
@@ -93,7 +85,7 @@ while flag_this < flag_jobs:
     # check job status
     jobs_status = check_status(jobs_status,jobs_ids)
     while len(jobs_status[jobs_status==1]) == flag_max:
-        time.sleep(600)
+        time.sleep(60)
         jobs_status = check_status(jobs_status,jobs_ids)
     flag_this += 1
 
