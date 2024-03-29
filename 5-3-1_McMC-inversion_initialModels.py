05aee5c760e46d35c719f10e17e8aadb00ad1977
Harmon at Dell-WSL <2947438278@qq.com>
2024-03-29 22:10:49 +0800

    SGB v9.5.6: plot for model validation

---
+++ 5-3-1_McMC-inversion_initialModels.py
@@ -0,0 +1,186 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import yaml
+import shutil
+
+# cd /shdisk/rem2/Harmon/F-J/San/project_repartition_v4.0/output_repar_v9.2_01-01/inv_dispernet/
+# mpirun -np 60 /home/songsh/git_repo/F-J/tools_F-J/toollib_DisbaCode/inversion.py --num_init 10  --config config_inv.yml --key select_50_60
+
+# %% [markdown]
+# ### Parameters
+
+# %% [markdown]
+# Vs:  Mordret 2019
+# Vp/Vs: Zigone 2019 (3-4)     (0-100m)
+# Density: Zigone 2019 (2-2.5) (0-100m)
+# 
+# 
+# Vp: Share 2020 (0.6-2.6) (0-70m)
+
+# %% [markdown]
+flag_project = 1 # 0--regular; 1--repartition
+# %%
+if flag_project == 0:
+    file_project = 'a-project.yml'
+elif flag_project == 1:
+    file_project = 'a-project_repar.yml'
+elif flag_project == 2:
+    file_project = 'a-project_voro.yml'
+    
+with open(file_project, 'r', encoding='utf-8') as f:
+    proj = yaml.load(f.read(), Loader=yaml.FullLoader)
+name_project = proj['name']
+
+#name_project = 'project/output_FJSJ_16-01/'               
+#name_project = 'project_repartrition/repartrition_01-03/'               
+#name_project = 'project_voronoi/voronoi_01-03/'         
+
+#%%
+with open('0_config.yml', 'r', encoding='utf-8') as f:
+    dir_config = yaml.load(f.read(), Loader=yaml.FullLoader)
+dir_project_workspace = dir_config['dir_project_workspace']
+dir_CC_workspace = dir_config['dir_CC_workspace']
+print('dir_CC_workspace: ', dir_CC_workspace)
+print('dir_project_workspace: ', dir_project_workspace)
+dir_project = os.path.join(dir_project_workspace, name_project)
+print('dir_project: ', dir_project)
+
+#%%
+filename = dir_project+'Basic_info.yml'
+with open(filename, 'r', encoding='utf-8') as f:
+    info_basic = yaml.load(f.read(), Loader=yaml.FullLoader)
+filename_bi = dir_project+'Basic_info.npy'
+info_basic_bi = np.load(filename_bi, allow_pickle='TRUE').item()      # setting dictionary
+
+
+# %%
+rdir_inv_McMC = 'inverion_McMC/'
+dir_inv_McMC = dir_project + rdir_inv_McMC
+info_basic['rdir_inv_McMC'] = rdir_inv_McMC
+dir_file = dir_inv_McMC + 'initial/'
+dir_image = dir_project + info_basic['rdir_image'] + 'initial_model/'
+dir_inv = dir_inv_McMC + 'inversion/'
+if os.path.exists(dir_image) == False:
+    os.makedirs(dir_image)
+if os.path.exists(dir_inv_McMC) == False:
+    os.makedirs(dir_inv_McMC)
+if os.path.exists(dir_file) == False:
+    os.makedirs(dir_file)
+if os.path.exists(dir_inv) == False:
+    os.makedirs(dir_inv)
+
+#%%
+#将config_inv.yml从San_Jansinto复制到
+outname_config = dir_inv + 'config_McMC.ini'
+if os.path.exists(outname_config)==False:
+    shutil.copyfile('config_McMC.ini', dir_inv_McMC+'config_McMC.ini')
+
+# %%
+V0 = 0.3
+alpha = 0.25
+#beta = 3
+#A1 = 0.4
+#A2 = 0.1
+d0 = 0.0
+
+# %%
+def Vs_model(V0,alpha,d0,d):
+    Vs = V0 * ((d*1e3+1)**alpha - (d0*1e3+1)**alpha + 1)
+    return Vs
+        
+
+# %%
+def Vs_model_linear(V0,k,d):
+    return V0 + k*d
+        
+
+# %%
+def brocher(z, vs):
+    vp = np.zeros(len(z))
+    rho = np.zeros(len(z))
+    for i in range(len(vs)):
+        vp[i] = (0.9409 + 2.0947 * vs[i] - 0.8206 * vs[i]**2 +
+              0.2683 * vs[i]**3 - 0.0251 * vs[i]**4)
+        rho[i] = (1.6612 * vp[i] - 0.4721 * vp[i]**2 + 0.0671 * vp[i]**3
+               - 0.0043 * vp[i]**4 + 0.000106 * vp[i]**5)
+    return vp,rho
+
+
+def gardner(z, vs):
+    vp = np.zeros(len(z))
+    rho = np.zeros(len(z))
+
+    vp_vs_ratio = 1.7321
+    vp = vp_vs_ratio * vs
+    rho = 1.741 * vp ** 0.25
+
+    return vp,rho
+
+
+def user_defined(z, vs):
+    vp = np.zeros(len(z))
+    rho = np.zeros(len(z))
+    vp_vs_ratio = 1.5
+    vp = vp_vs_ratio * vs
+    for i in range(len(vs)):
+        vp[i] = (0.9409 + 2.0947 * vs[i] - 0.8206 * vs[i]**2 +
+              0.2683 * vs[i]**3 - 0.0251 * vs[i]**4)
+        rho[i] = (1.6612 * vp[i] - 0.4721 * vp[i]**2 + 0.0671 * vp[i]**3
+               - 0.0043 * vp[i]**4 + 0.000106 * vp[i]**5)
+        
+    return vp,rho
+
+# %%
+N = 100
+dz = 0.003
+flag_relation = 1
+
+# %%
+layers = np.linspace(1,N,N)
+depths = np.zeros(N)
+for i in range(N):
+    depths[i] = d0 + i*dz
+Vp = np.zeros(N)
+Vs = np.zeros(N)
+rho = np.zeros(N)
+for i in range(N):
+    d = depths[i]
+    Vs[i] = Vs_model(V0,alpha,d0,d)
+if flag_relation == 1:
+    Vp,rho = brocher(depths,Vs)
+elif flag_relation == 2:
+    Vp,rho = gardner(depths,Vs)
+elif flag_relation == 3:
+    Vp,rho = user_defined(depths,Vs)
+
+# %%
+def plot_initial(Vp,Vs,rho,depths,tag):
+    plt.figure(figsize=(8,6))
+    plt.plot(Vp,depths,'r',label='Vp')
+    plt.plot(Vs,depths,'b',label='Vs')
+    plt.plot(rho,depths,'g',label='rho')
+    plt.legend()
+    # 翻转y轴
+    plt.gca().invert_yaxis()
+    plt.xlim(0,4)
+    plt.savefig(dir_image+'initial_model_'+str(tag)+'.png')
+    #plt.close()
+
+# %%
+# write a function to write the initial model as txt file, which seperate by space，with four significant digits
+def write_initial_model(dir_file,layers,depths,Vp,Vs,rho,tag):
+    with open(dir_file+'/initial_model_'+str(tag)+'.txt','w') as f:
+        for i in range(len(layers)):
+            f.write('{} {:.4f} {:.4f} {:.4f} {:.4f}\n'.format(int(layers[i]),depths[i],Vp[i],Vs[i],rho[i]))
+
+# %%
+tag = 1
+flag_plot = 1
+write_initial_model(dir_file,layers,depths,Vp,Vs,rho,tag)
+if flag_plot == 1:
+    plot_initial(Vp,Vs,rho,depths,tag)
+
+with open(dir_project+'Basic_info.yml', 'w', encoding='utf-8') as f:
+   yaml.dump(data=info_basic, stream=f, allow_unicode=True)
---
+++ 7-2_modelvalidate_ReadCcfs2h5.py
@@ -0,0 +1,87 @@
+import os,pandas,h5py,itertools
+import numpy as np
+from geopy.distance import great_circle
+from multiprocessing import Manager
+import matplotlib.pyplot as plt
+from multiprocessing import Pool as ThreadPool
+import yaml
+import pandas as pd
+
+# %%
+with open('0_config.yml', 'r', encoding='utf-8') as f:
+    dir_config = yaml.load(f.read(), Loader=yaml.FullLoader)
+dir_SAC_workspace = dir_config['dir_SAC_workspace']
+dir_CC_workspace = dir_config['dir_CC_workspace']
+print('dir_SAC_workspace: ', dir_SAC_workspace)
+print('dir_CC_workspace: ', dir_CC_workspace)
+
+#%% 
+name_CC = 'CC/CC_40_prewhiten/'  
+dir_CC = os.path.join(dir_CC_workspace,name_CC)
+filename = dir_CC+'info_CC.npy'
+info_CC = np.load(filename, allow_pickle='TRUE').item()
+
+#%% 
+outpath = dir_CC + 'CFs_modelvalidate/'
+out_file = outpath + 'gather_all_modelvalidate.h5'
+if not os.path.exists(outpath):
+    os.mkdir(outpath)
+
+#%%
+stalist = []
+lon = []
+lat = []
+stalistname_all = info_CC['stalistname']
+stainfo = pd.read_excel(stalistname_all)
+stalist = stainfo['Station'].tolist() 
+lat =  stainfo['latitude'].tolist() 
+lon =  stainfo['longitude'].tolist() 
+
+#%% 
+original_stack = h5py.File(dir_CC + 'gather_all.h5','r')
+R = original_stack['r'][:]
+Stalist = stalist
+Ncfs = original_stack['ncfs'][:]
+StationPairs = original_stack['StationPairs'][:]
+
+#%% 
+CC_name = []
+Dis_great=[]
+CC_data = []
+sta0_lon_lat0=[]
+sta1_lon_lat0=[]
+jj = 0
+for i in range(len(R)):
+    sta0 = Stalist[StationPairs[i*2]]
+    sta1 = Stalist[StationPairs[i*2+1]]
+    if (sta0 in stalist and sta1 in stalist):
+        cc_name = sta0 + '_' + sta1
+        CC_name.append(cc_name)
+        id_sta0 = stalist.index(sta0)
+        id_sta1 = stalist.index(sta1)
+        sta0_lon_lat0.append([float(lon[id_sta0]),float(lat[id_sta0])])
+        sta1_lon_lat0.append([float(lon[id_sta1]),float(lat[id_sta1])])
+        Dis_great.append(R[i])
+        CC_data.append(Ncfs[i])
+        jj = jj+1
+        print(str(jj)+' of '+str(len(R)))
+
+                    
+
+r0 = np.array(Dis_great)
+ncfs0 = np.array(CC_data)
+#-------------------------------------------------------------------------------#
+cc_prof = pandas.DataFrame({'0cc_name': CC_name,
+                            '1sta0': sta0_lon_lat0,
+                            '2sta1': sta1_lon_lat0,
+                            '3dis_great': r0,
+                            })
+
+freq = pandas.DataFrame({'f': original_stack["f"]})
+if os.path.exists(out_file):
+    os.remove(out_file)
+f = h5py.File(out_file,'a')
+f.create_dataset('all', data=ncfs0)
+f.close()
+cc_prof.to_hdf(out_file, key='cc_prof',mode = 'r+')
+freq.to_hdf(out_file, key='freq',mode = 'r+')
